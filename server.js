53edce24d544b4c6c1d7feedb5a0433e8f4e9d22
chriscasella <chris.casella@gmail.com>
2018-04-25 23:24:37 -0400

    fixed artist parsing. RegExp used on artist, splits until it h its a hyphen and then passes whats before it to the search. works!

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const testsubStreamOpts = {
 
 const comments = client.CommentStream(streamOpts);
 
-const submissions = client.SubmissionStream(testsubStreamOpts);
+const submissions = client.SubmissionStream(subStreamOpts);
 
 comments.on('comment', (comment)=>{
     console.log(comment);
@@ -60,8 +60,23 @@ let currentSubmission = null;
 
 let isSong = (submission) => {
     let title = submission.title;
+    const pattern = new RegExp(/\s*-\s*/);
+    title.match(pattern) ? parseTitle(title) : ''
+    //let split = title.split(' ');
+    //split[1] == '-' ? searchSimilarArtists(split[0]) : ''//nothing   
+};
+
+let parseTitle = (title) => {
     let split = title.split(' ');
-    split[1] == '-' ? searchSimilarArtists(split[0]) : ''//nothing   
+    let titleArr = [];
+    for(i of split){
+        if(i == '-'){
+            let joinedTitle = titleArr.join(' ');
+            searchSimilarArtists(joinedTitle);
+        }else if( i != '-'){
+            titleArr.push(i);
+        };
+    };
 };
 
 let searchSimilarArtists = (artist) => {
@@ -105,19 +120,21 @@ let parseYtResponse =(ytObj)=>{
     };
     commentPayload.push(ytVidObj); 
     counter++;
-    counter == 3 ? (postComment(), counter = 0 ) : '';
+    counter == 3 ? postComment() : '';
 };
 
 let postComment = () =>{
-    let commentText = '**Here are some similar artists for this submission** &nbsp * [' + commentPayload[0].artist + '](' + commentPayload[0].link + ') &nbsp + [' + commentPayload[1].artist + '](' + commentPayload[1].link + ') &nbsp - [' + commentPayload[2].artist + '](' + commentPayload[2].link +') &nbsp *I am a bot, sent here to spread prog metal*' ;
+    console.log('commentPayload',commentPayload,'savedArtists', savedArtists);
+    let commentText = '**Here are some similar artists for this submission** \n\n &nbsp; [' + commentPayload[0].artist + '](' + commentPayload[0].link + ') \n\n &nbsp; [' + commentPayload[1].artist + '](' + commentPayload[1].link + ') \n\n &nbsp; [' + commentPayload[2].artist + '](' + commentPayload[2].link +')  \n\n &nbsp; *I am a bot, sent here to spread prog metal* d(>_<)b' ;
     currentSubmission.reply(commentText);
     wipeVars();
 };
 
 let wipeVars = ()=>{
-    let savedArtists = [];
-    let commentPayload = [];
-    let currentSubmission = null;
+    savedArtists = [];
+    commentPayload = [];
+    currentSubmission = null;
+    counter = 0;
 };
 
 const testData = [
