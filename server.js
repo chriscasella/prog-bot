a01fc1c64c2d270c086bc9f872392b1bf21b039c
chriscasella <chris.casella@gmail.com>
2018-04-28 01:02:26 -0400

    added get req for /

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ const app = express();
 const lastSecret = process.env.LAST_SECRET;
 const lastApi = process.env.LAST_API;
 
-app.listen(process.env.PORT || 5000, ()=>{console.log('listening to port 5000')});
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function (request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function () {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 const r = new Snoowrap({
     userAgent: 'prog-bot',
